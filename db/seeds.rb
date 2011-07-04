716830f76328e1710c7a2caeed2a40f643173d93
Mehigh <me@mehigh.biz>
2011-07-04 18:06:01 +0300

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
-admin = User.create!(:username => 'admin', :email => 'admin@mehigh.biz', :password => '123456', :password_confirmation => '123456', :role => 'admin')
-newcompany = Company.create!(:name => 'Owner', :country => 'Romania', :slug => 'own')
-admin.company = newcompany
+# admin = User.create!(:username => 'admin', :email => 'admin@mehigh.biz', :password => '123456', :password_confirmation => '123456', :role => 'admin')
+# newcompany = Company.create!(:name => 'Owner', :country => 'Romania', :slug => 'own')
+admin.company = Company.first
+admin.save
